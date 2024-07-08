9be9e227d2611133515ac914154c9aa00bda51b5
Jayy <jj.17.10.07@gmail.com>
2024-07-08 03:30:48 +0200

    test

---
+++ server.lua
@@ -0,0 +1,18 @@
+CreateThread( function()
+    local version = GetResourceMetadata(GetCurrentResourceName(), 'version')
+    Citizen.Wait(5000)
+    PerformHttpRequest('https://raw.githubusercontent.com/0jayy/version.json', function(code, res, headers)
+        if code == 200 then
+            local response = json.decode(res)
+            if response.version ~= version then
+              print(([[^1-------------------------------------------------------
+ Update! Version: %s
+
+ Changlog: %s
+ -------------------------------------------------------^0]]):format(response.version, response.changelog))
+           end
+       else
+           print('[^1ERROR^7] >> Failed to fetch version information (HTTP code: %s)'):format(code))
+       end
+   end, 'GET')
+ end
