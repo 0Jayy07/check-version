b9b802c8ba0f86ac02747440a33c9dfa43ee38c1
Jayy <96948432+0Jayy07@users.noreply.github.com>
2024-09-14 21:29:04 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,13 +6,13 @@ CreateThread( function()
             local response = json.decode(res)
             if response.version ~= version then
               print(([[^1-------------------------------------------------------
- Update! Version: %s
+ ^2Update!^7 ^4Version: %s^7
 
- Changlog: %s
+ ^2Changlog:^7 ^6%s^7
  -------------------------------------------------------^0]]):format(response.version, response.changelog))
            end
        else
-           print('[^1ERROR^7] >> Failed to fetch version information (HTTP code: %s)'):format(code))
+           print('[^1ERROR^7] >> Failed to get version information. ^1ERROR: %s^7'):format(code))
        end
    end, 'GET')
  end
